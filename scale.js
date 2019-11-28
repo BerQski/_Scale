5c7d2cdd55a62fa450bfcd5f26dfa90cd1066777
Bern <bernard.kulegowski@gmail.com>
2019-11-28 20:46:59 +0100

    ze swichem

--- scale.js
+++ scale.js
@@ -1,4 +1,3 @@
-let array = [0, 1, 0, 0, 0, 0, 0, 0];
 // let z = [];?
 // function untillEnd(aRa, cOn) {
 //   let end = aRa.length;
@@ -31,3 +30,63 @@ let array = [0, 1, 0, 0, 0, 0, 0, 0];
 //     return szalka2.flat();
 //   }
 // }
+let array = [1, 1, 1, 1, 1, 1, 1, 3];
+let szalka1 = [];
+let szalka2 = [];
+let szalka3 = [];
+function UntillEnd(aRa, cOn, ff) {
+  let i = 1;
+  while (i <= cOn) {
+    ff.push(aRa.splice(Math.floor(Math.random() * aRa.length), 1));
+    i++;
+  }
+  return ff.flat();
+}
+
+console.log(UntillEnd(array, 3, szalka1));
+console.log(UntillEnd(array, 3, szalka2));
+console.log(UntillEnd(array, 3, szalka3));
+// // console.log(array);
+// function comPare(szal1, szal2, szal3) {
+//   const a = szal1.reduce((a, b) => a + b);
+//   const b = szal2.reduce((a, b) => a + b);
+//   const c = szal3.reduce((a, b) => a + b);
+//   if (a == b) {
+//     if (szal3[0] > szal3[1]) {
+//       return szal3[0];
+//     } else {
+//       return szal3[1];
+//     }
+//   } else if (a < b) {
+//     if (szal2[0] > szal2[1]) {
+//       return szal2[0];
+//     } else if (szal2[2] > szal2[1]) {
+//       return szal2[2];
+//     } else {
+//       return szal2[1];
+//     }
+//   } else {
+//     if (szal1[0] > szal1[1]) {
+//       return szal1[0];
+//     } else if (szal1[2] > szal1[1]) {
+//       return szal1[2];
+//     } else {
+//       return szal1[1];
+//     }
+//   }
+// }
+// console.log(comPare(szalka1, szalka2, szalka3));
+function ComPareTwo(s1, s2, s3) {
+  const a = s1.reduce((a, b) => a + b);
+  const b = s2.reduce((a, b) => a + b);
+
+  switch (a > b || b > a || a == b) {
+    case a > b == true:
+      return s1.flat();
+    case b > a == true:
+      return s2.flat();
+    default:
+      return s3.flat();
+  }
+}
+console.log(ComPareTwo(szalka1, szalka2, szalka3));
